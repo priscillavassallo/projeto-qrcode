a7f60d5753ff84979a3fe26cdf1e9b8115897d8d
priscillavassallo <priscillavassallo96@gmail.com>
2025-12-18 13:44:53 -0300

    adiciona qr-code architecture

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import prompt from "prompt";
 
 import mainPrompt from "./prompts/prompt-main.js";
+import createQRCode from "./services/qr-code/create.js";
 
 async function main() {
     prompt.get(mainPrompt, async (error, choose) => {
