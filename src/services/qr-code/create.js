6b6516b1ccb0e825083e0dfb8ac679c9e2df0032
priscillavassallo <priscillavassallo96@gmail.com>
2025-12-18 14:14:24 -0300

    adiciona qr-code generate

--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,6 +1,10 @@
 import prompt from "prompt";
 import promptQRCode from "../../prompts/prompt-qrcode";
+import handle from "./handle.js";
 
-async function createQRCode() {}
+async function createQRCode() {
+    prompt.get(promptQRCode, handle);
+    prompt.start();
+}
 
 export default createQRCode;
